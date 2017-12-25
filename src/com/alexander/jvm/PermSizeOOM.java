61debf360ca9008348d2de8f9d246e2553e5aa28
Administrator <Administrator@192.168.0.103>
2017-12-25 22:29:52 +0800

    -XX:MaxPermSize=2M works -XX:MaxMetaspaceSize=2M does not work.

--- src/com/alexander/jvm/PermSizeOOM.java
+++ src/com/alexander/jvm/PermSizeOOM.java
@@ -7,22 +7,34 @@ import java.util.Map;
 
 public class PermSizeOOM {
 	
-	public static void main(String[] args){
-		
-		String s1 = "Alexander";
-		
-		Map<String,String> temp = new HashMap<String,String>();
-		temp.put("1", "Alex");
-		temp.put("2", "ander");
-		String s2 = temp.get("1") + temp.get("2");
-		System.out.println("s1==s2 is: " + s1==s2);
-		
+	static String prefix = "Alex";
+	static String suffix = "ander";
+
+	public static void main(String[] args) {
+
+		// String s1 = "Alexander";
+
+		// Map<String,String> temp = new HashMap<String,String>();
+		// temp.put("1", "Alex");
+		// temp.put("2", "ander");
+		// String s2 = temp.get("1") + temp.get("2");
+		// System.out.println("s2 : " + s2);
+		// System.out.println("s1==s2 is: " + s1==s2);
+		//
+		// List<String> list = new ArrayList<String>();
+		// int i = 0;
+		// while(true){
+		// System.out.println(i);
+		// list.add(String.valueOf(i++).intern());
+		// }
+
 		List<String> list = new ArrayList<String>();
-		int i = 0;
-		while(true){
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
 			System.out.println(i);
-			list.add(String.valueOf(i++).intern());
+			String str = prefix + suffix;
+			prefix = str;
+			list.add(str.intern());
 		}
-		
+
 	}
 }
